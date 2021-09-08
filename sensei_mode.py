e2f7751604177a13235dc7be2305d4b45b32b582
Amani AbuQdais <64901677+aq-amani@users.noreply.github.com>
2021-09-08 10:27:18 +0900

    some explanation additions/fixes

--- sensei_mode.py
+++ sensei_mode.py
@@ -22,7 +22,7 @@ First let us start with the nature of sound.
 As you already know from grade school,
 sound is the way our brains perceive vibrations that travel through air or any other medium.
 
-Those vibrations can be represented as waves of certain frequencies.
+These vibrations can be represented as waves of certain frequencies.
 The simplest form of sound can be created using a single sine wave.
 Here is how a single sine wave of XXHz frequency sounds like..
 """
@@ -36,25 +36,32 @@ See how XXHz wave/note compares to YYHz and ZZHz waves/notes.
 I will replace the word wave with [note] instead hereafter.
 """
 
-note_exp3 = """
-To distinguish notes in a human-friendly way,
-notes were given names using english letters from A to F.
-Here are the basic notes that have names, along with their frequencies.
-The frequency ratio between each of these notes is 2^12. This ratio is called a [Half step] or a [Semi-tone].
-Two half steps make a [Whole step] or a [Full Tone].
-"""
-
 octave_exp = """
 Doubling the frequency of a note will produce a sound that our brain perceives as similar but of a higher pitch.
-Such notes have the same name but exist on a higher [octave].
+Such notes have the same name but exist on different [octave]s.
 See how XXHz and ZZHz sound like.
 """
+
+octave_exp2 = """
+People historically tried to divide the frequencies between notes and their next octave counterpart in many different ways.
+Some division systems assumed equal spacing between notes, while others did not.
+The most common system is one that divides the interval between a note and its octave counter part into [12] logarithmically equal intervals.
+That is, each note frequency is equal to the previous note frequency multiplied by 2^(1/12).
+
+This 2^(1/12) ratio between consecutive frequencies is called a [Half step] or a [Semi-tone].
+Two half steps/ semi-tones make a [Whole step] or a [Full Tone].
+
+Each note on these intervals is given a human-friendly name, an alphabet between A~F, instead of just a frequency values to refer to these notes.
+Here are the basic notes that have names, along with their frequencies.
+"""
+
+
+
 
 sensei_print(intro)
 sensei_print(note_exp1)
 #play_note()
 sensei_print(note_exp2)
 #play_note_compare()
-sensei_print(note_exp3)
 sensei_print(octave_exp)
-
+sensei_print(octave_exp2)
